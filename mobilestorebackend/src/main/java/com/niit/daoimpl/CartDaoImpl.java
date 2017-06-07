7e40d410cdb82c244465d6a5004ebd10c8631af4
shakirkhan06 <shakirkhan06@gmail.com>
2017-06-07 13:28:21 +0530

    Updated on 07/06/2017
    Project Final Update

---
+++ mobilestorebackend/src/main/java/com/niit/daoimpl/CartDaoImpl.java
@@ -0,0 +1,91 @@
+package com.niit.daoimpl;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.niit.dao.CartDao;
+import com.niit.model.CartModel;
+
+@Repository
+public class CartDaoImpl implements CartDao{
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	public void setSessionFactory(SessionFactory sf){
+		this.sessionFactory = sf;
+	}
+
+	public void save(CartModel cart1)
+	{
+		Session s=sessionFactory.openSession();
+		s.beginTransaction();
+		s.save(cart1);
+		s.getTransaction().commit();
+		s.close();
+		
+	}
+
+	public CartModel getId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+/*	public void delete(CartModel p)
+	{
+		
+		Session s=sessionFactory.openSession();
+		s.beginTransaction();
+		s.delete(p);
+		s.getTransaction().commit();
+		s.close();
+
+	} */ 
+	public List<CartModel> getAll() 
+	{
+		Session s=sessionFactory.openSession();
+		List<CartModel> clist=s.createCriteria(CartModel.class).list();
+		s.clear();
+		return clist;
+	}
+	public CartModel findById(int id) 
+	{
+		return (CartModel)sessionFactory.openSession().get(CartModel.class,id);
+	}
+	public List<CartModel> checkExistance(String pid) 
+	{
+        Session s=sessionFactory.openSession();		
+        List<CartModel> results =s.createQuery("from CartModel where prodId="+pid).list();		
+		s.close();
+		return results;
+	}
+
+
+	public void update(CartModel p) 
+	{
+		Session s=sessionFactory.openSession();
+		s.beginTransaction();
+		s.update(p);
+		s.getTransaction().commit();
+		s.close();
+	}
+	
+	@Override
+	public void deleteById(CartModel cart) {
+		Session s=sessionFactory.openSession();
+		s.beginTransaction();
+		s.delete(cart);
+		s.getTransaction().commit();
+		s.close();
+	}
+
+	      
+		
+}
